77f5fc31b3c98e3a5d9204fa588d4fe258928d88
Paula <Paula.Castillejo1@alu.uclm.es>
2023-11-15 11:57:16 +0100

    posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,17 +3,17 @@ var mongoose = require('mongoose');
 var router = express.Router();
 var Post = require('../models/Posts');
 var User = require('../models/User');
-var db = mongoose.connection();
+var db = mongoose.connection;
 
 router.get("/",
 function (req, res, next) {
-    Post.find().sort("-publicationdate").populate('user').exec(function (err, posts) {
+    Post.find().sort("-publicationdate").exec(function (err, posts) {
         if (err) res.status(500).send(err);
         else res.status(200).json(posts);
     })
 });
 
-router.get("/all/:id", tokenVerify, function (req, res, next) {
+router.get("/all/:id", function (req, res, next) {
     User.create(req.body).sort("-publicationdate").populate('user').exec(function (err, posts) {
         if (err) res.status(500).send(err);
         else res.send(200).json(posts);
